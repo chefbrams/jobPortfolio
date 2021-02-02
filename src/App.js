5a0495635ec3a030713a70cb2a99e256ff9a38e7
chefbrams <oldcitycatering@gmail.com>
2021-02-02 10:19:25 -0500

    added header component with multiple packages and modified navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import './App.css';
-import $ from "jquery";
+import "jquery/dist/jquery.min.js";
 import Popper from "popper.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
-
+import Header from "./components/Header";
+import Particles from 'react-particles-js';
 function App() {
   return (
-    
+    <>
+    <Particles
+    className="particles-canvas"
+    params={{
+      particles: {
+        number:{
+          value: 30,
+          density: {
+            enable: true,
+            value_area: 900
+          }
+        },
+        shape: {
+          type: "star",
+          stroke: {
+            width: 6,
+            color: "#219ebc"
+          }
+
+        }
+      }
+    }} />
     <Navbar />
+    <Header />
+    </>
     
   );
 }
