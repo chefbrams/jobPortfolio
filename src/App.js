03bca82ed78c05cc4207a59003ece81126fb2791
chefbrams <oldcitycatering@gmail.com>
2021-02-09 19:48:54 -0500

    removed contact button on home page, removed jquery package and added cdn, hamburger works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
-import $ from "jquery";
-import Popper from "popper.js";
+import "./App.css";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-import Particles from 'react-particles-js';
+import Particles from "react-particles-js";
 import AboutMe from "./components/AboutMe";
 import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
@@ -13,36 +12,35 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <>
-    <Particles
-    className="particles-canvas"
-    params={{
-      particles: {
-        number:{
-          value: 30,
-          density: {
-            enable: true,
-            value_area: 900
-          }
-        },
-        shape: {
-          type: "star",
-          stroke: {
-            width: 6,
-            color: "#219ebc"
-          }
-
-        }
-      }
-    }} />
-    <Navbar />
-    <Header />
-    <AboutMe />
-    <Skills />
-    <Portfolio />
-    <Contact />
-    <Footer />
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900,
+              },
+            },
+            shape: {
+              type: "star",
+              stroke: {
+                width: 6,
+                color: "#219ebc",
+              },
+            },
+          },
+        }}
+      />
+      <Navbar />
+      <Header />
+      <AboutMe />
+      <Skills />
+      <Portfolio />
+      <Contact />
+      <Footer />
     </>
-    
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,20 @@
 import React from "react";
-import Typed from 'react-typed';
+import Typed from "react-typed";
 const Header = () => {
-    return (
-        <div id="header" className="header-wrapper">
-            <div className="main-info">
-                <h1>Full Stack Developer</h1>
-                <Typed className="typed-text" 
-                strings={["MongoDB", "Express", "ReactJS", "NodeJS"]}
-                typeSpeed={40}
-                backSpeed={60}
-                loop
-                />
-                 <a href="#" className="btn-main-offer">Contact Me</a>  
-            </div>
-        </div>
-    )
-}
+  return (
+    <div id="header" className="header-wrapper">
+      <div className="main-info">
+        <h1>Full Stack Developer</h1>
+        <Typed
+          className="typed-text"
+          strings={["MERN Stack", "MongoDB", "Express", "ReactJS", "NodeJS"]}
+          typeSpeed={40}
+          backSpeed={60}
+          loop
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
